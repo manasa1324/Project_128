281fad6906a7cc9ccae092b2f63d4c3833fda046
manasa1324 <79976512+manasa1324@users.noreply.github.com>
2021-11-23 11:54:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+    song1 = loadSound("Harry_Potter_Theme_Song.mp3");
+    song2 = loadSound("Peter_Pan_Music.mp3");
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song2.play();
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + ", leftwristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + ", rightwristY = " + rightWristY);
+    }
+}
